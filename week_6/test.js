1e6be61ff0057119137a9096124940d51624c7a8
Pantakarn64309010022 <98859750+zNithan@users.noreply.github.com>
2022-11-24 15:28:12 +0700

    Create test.js

---
+++ week_6/test.js
@@ -0,0 +1,64 @@
+let data =[{
+    "employeeID" : 10001,
+    "name":"นางสาวชนิสา ไกรรอด",
+    "department":"ไอที",
+    "role":"System Engineer",
+    "salary":42000,
+    "bonus": "S"
+},{
+    "employeeID" : 10002,
+    "name":"นายวศพล สิริอนันต์",
+    "department":"ไอที",
+    "role":"System Engineer",
+    "salary":13000,
+    "bonus": "A"
+},{
+    "employeeID" : 10003,
+    "name":"นางจิราพร อินทรประเสริฐ",
+    "department":"ฝ่ายบุคคล",
+    "role":"HR",
+    "salary":9000,
+    "bonus": "C"
+},{
+    "employeeID" : 10004,
+    "name":"นางสาวพิมพา กาญจนมาศ",
+    "department":"ฝ่ายบุคคล",
+    "role":"HR",
+    "salary":21000,
+    "bonus": "B"
+},{
+    "employeeID" : 10005,
+    "name":"นายจิรโชติ วัฒนโกศล",
+    "department":"ฝ่ายบุคคล",
+    "role":"HR",
+    "salary":22000,
+    "bonus": "A"
+},{
+    "employeeID" : 10006,
+    "name":"นายธัชพล ขจรเกียรติสกุล",
+    "department":"การเงิน",
+    "role":"Account",
+    "salary":12000,
+    "bonus": "S"
+},{
+    "employeeID" : 10007,
+    "name":"นายธนวิทย์ สุวรรณโชติ",
+    "department":"การเงิน",
+    "role":"Account",
+    "salary":14000,
+    "bonus": "A"
+},{
+    "employeeID" : 10008,
+    "name":"นางพัชรียา สําราญทรัพย์",
+    "department":"การเงิน",
+    "role":"Account",
+    "salary":50000,
+    "bonus": "C"
+},{
+    "employeeID" : 10009,
+    "name":"นางสาวขวัญชนก ธาดาวรวงศ์",
+    "department":"ไอที",
+    "role":"System Engineer",
+    "salary":16000,
+    "bonus": "A"
+}];
